0b9ccfa734632e2c81f0c0f944f98607b063c75a
AnthonyDoyle6 <20088327@mail.wit.ie>
2022-02-22 10:15:54 +0000

    Added filtering of movie list.

--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -22,6 +22,24 @@ const MovieListPage = (props) => {
       });
   }, []);
 
+  const [nameFilter, setNameFilter] = useState("");
+  const [genreFilter, setGenreFilter] = useState("0");
+
+  const genreId = Number(genreFilter);
+
+  let displayedMovies = movies
+    .filter((m) => {
+      return m.title.toLowerCase().search(nameFilter.toLowerCase()) !== -1;
+    })
+    .filter((m) => {
+      return genreId > 0 ? m.genre_ids.includes(genreId) : true;
+    });
+
+  const handleChange = (type, value) => {
+    if (type === "name") setNameFilter(value);
+    else setGenreFilter(value);
+  };
+
   return (
     <Grid container sx={{ padding: "20px" }}>
       <Grid item xs={12}>
@@ -31,7 +49,7 @@ const MovieListPage = (props) => {
         <Grid key="find" item xs={12} sm={6} md={4} lg={3} xl={2}>
           <FilterCard />
         </Grid>
-        <MovieList movies={movies}></MovieList>
+        <MovieList movies={displayedMovies} />
       </Grid>
     </Grid>
   );
